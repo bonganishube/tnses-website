080c96e069e0587bc03c89b35b4f7aade9934b8a
Bongani Shube <bonganishubeaz@gmail.com>
2024-11-07 20:56:24 +0200

    Redesign services section

--- front-end/src/components/services/Services.jsx
+++ front-end/src/components/services/Services.jsx
@@ -1,74 +1,45 @@
-import React from "react";
-import './Services.css';
-import { MdCastForEducation } from "react-icons/md";
-import { MdOutlineMenuBook } from "react-icons/md";
-import { FaChalkboardTeacher } from "react-icons/fa";
-
-const skillsData = [
-  {
-    name: "Google for Education",
-    icon: <MdCastForEducation className="service-icon" />,
-    link: "#",
-    description:
-      "ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod metus vel sem bibendum, a bibendum justo tempor. Sed vel lectus",
-    aosDelay: "0",
-  },
-  {
-    name: "Blueprint",
-    icon: <MdOutlineMenuBook className="service-icon" />,
-    link: "#",
-    description:
-      "ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod metus vel sem bibendum, a bibendum justo tempor. Sed vel lectus",
-    aosDelay: "300",
-  },
-  {
-    name: "Service Three",
-    icon: <FaChalkboardTeacher className="service-icon" />,
-    link: "#",
-    description:
-      "ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod metus vel sem bibendum, a bibendum justo tempor. Sed vel lectus",
-    aosDelay: "500",
-  },
-];
+import React from 'react';
+import './Services.css'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const Services = () => {
   return (
-    <div className='services'>
-      <span id="about"></span>
-      <div className="services-container">
-        <div className="container">
-          {/* Header */}
-          <div className="header">
-            <h1 className="header-title">Explore Our Services</h1>
-            <p className="header-description">
-            "ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod bibendum.
-            </p>
-          </div>
-
-          {/* services cards */}
-          <div className="services-grid">
-            {skillsData.map((skill) => (
-              <div
-                key={skill.name}
-                className="service-card"
-                data-aos="fade-up"
-                data-aos-delay={skill.aosDelay}
-              >
-                <div>{skill.icon}</div>
-                <h1 className="service-title">{skill.name}</h1>
-                <p className="service-description">{skill.description}</p>
-
-                {/* Subtle Learn More button for each service */}
-                <a href={skill.link} className="subtle-btn">
-                  Learn More
-                </a>
-              </div>
-            ))}
-          </div>
+    <div className='services-section' id='services'>;
+      <div className='services-container'>
+        <div>
+          <h2>
+            What We Can Do Together
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+          </p>
+        </div>
+        <div className='service-category'>
+            <Tabs>
+              <TabList className='tab-list'>
+                <Tab>Google for Education</Tab>
+                <Tab>Blueprint</Tab>
+                <Tab>Mentorship</Tab>
+              </TabList>
+                <div>
+                  <h3 claddName='google-for-education'>
+                    Google for Education
+                  </h3>
+                </div>
+              <TabPanel>
+                <h2>Any content 1</h2>
+              </TabPanel>
+              <TabPanel>
+                <h2>Any content 2</h2>
+              </TabPanel>
+              <TabPanel>
+                <h2>Any content 3</h2>
+              </TabPanel>
+            </Tabs>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
