fc3074b6409030698971a92603beb561f87801b4
Bongani Shube <bonganishubeaz@gmail.com>
2024-11-13 14:04:05 +0200

    Design contact

--- front-end/src/components/contact/Contact.jsx
+++ front-end/src/components/contact/Contact.jsx
@@ -1,75 +1,70 @@
 import React from 'react';
 import './Contact.css';
-import contactImg from '../../assets/services/blueprint.jpg';
 
 const Contact = () => {
   return (
     <section>
         <div className='contact-container'>
-            <div className='contact'>
+            <div className='contact-info'>
                 <div>
-                    <h2>
-                        Contact Info
-                    </h2>
+                    <h2>Contact Info</h2>
                     <ul className='info'>
                         <li>
-                            <span><img src={contactImg} alt=''/></span>
-                            <span>Belhar, Pentech, <br></br>
-                            Cape Town, <br></br>
-                            Western Cape <br></br>
+                            <span><i className="fa-solid fa-location-dot"></i></span>
+                            <span>Belhar, Pentech, <br />
+                            Cape Town, <br />
+                            Western Cape <br />
                             7493</span>
                         </li>
                         <li>
                             <span><i className="fa-solid fa-envelope"></i></span>
-                            <span><a href = 'mailto:info@tnses.org'>info@tnses.org</a></span>
+                            <span><a href='mailto:info@tnses.org'>info@tnses.org</a></span>
                         </li>
                         <li>
-                            <span><i className="fa-solid fa-phone"></i>+27 12 345 67-89</span>
+                            <span><i className="fa-solid fa-phone"></i> +27 12 345 67-89</span>
                             <span>+27 12 234 56-78</span>
                         </li>
                     </ul>
-                    <ul className ='sci'>
-                        <li><a href=''><i className='fab brands fa-facebook'></i></a></li>
-                        <li><a href=''><i className='fab brands fa-instagram'></i></a></li>
-                        <li><a href=''><i className='fab brands fa-twitter'></i></a></li>
-                        <li><a href=''><i className='fab brands fa-youtube'></i></a></li>
-                    </ul>
                 </div>
+                <ul className='sci'>
+                    <li><a href=''><i className='fab fa-facebook'></i></a></li>
+                    <li><a href=''><i className='fab fa-instagram'></i></a></li>
+                    <li><a href=''><i className='fab fa-twitter'></i></a></li>
+                    <li><a href=''><i className='fab fa-youtube'></i></a></li>
+                </ul>
             </div>
+
             <div className='contact-form'>
-                <h2>
-                    Send a Message
-                </h2>
+                <h2>Send a Message</h2>
                 <div className='form-box'>
-                    <div className='input-box'>
-                        <input type='text' name='' required>
-                        </input>
+                    <div className='input-box w50'>
+                        <input type='text' name='firstName' required />
                         <span>First Name</span>
                     </div>
-                    <div className='input-box'>
-                        <input type='text' name='' required></input>
+                    <div className='input-box w50'>
+                        <input type='text' name='lastName' required />
                         <span>Last Name</span>
                     </div>
-                    <div className='input-box'>
-                        <input type='text' name='' required></input>
+                    <div className='input-box w50'>
+                        <input type='email' name='email' required />
                         <span>Email Address</span>
                     </div>
-                    <div className='input-box'>
-                        <input type='text' name='' required></input>
+                    <div className='input-box w50'>
+                        <input type='tel' name='mobile' required />
                         <span>Mobile Number</span>
                     </div>
-                    <div className='input-box-message'>
-                        <textarea name='' required></textarea>
+                    <div className='input-box w100'>
+                        <textarea name='message' required></textarea>
                         <span>Write your message here</span>
                     </div>
-                    <div className='input-box'>
-                        <input type='submit' value='send'></input>
+                    <div className='input-box w100'>
+                        <input type='submit' value='Send' />
                     </div>
                 </div>
             </div>
         </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
