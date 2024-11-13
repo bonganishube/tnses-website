68a97ba78858b4b96ea5763af4157b5bc00e1abf
Bongani Shube <bonganishubeaz@gmail.com>
2024-11-13 20:07:02 +0200

    Design footer section

--- front-end/src/components/contact/Contact.jsx
+++ front-end/src/components/contact/Contact.jsx
@@ -4,67 +4,88 @@ import './Contact.css';
 const Contact = () => {
   return (
     <section>
-        <div className='contact-container'>
-            <div className='contact-info'>
-                <div>
-                    <h2>Contact Info</h2>
-                    <ul className='info'>
-                        <li>
-                            <span><i className="fa-solid fa-location-dot"></i></span>
-                            <span>Belhar, Pentech, <br />
-                            Cape Town, <br />
-                            Western Cape <br />
-                            7493</span>
-                        </li>
-                        <li>
-                            <span><i className="fa-solid fa-envelope"></i></span>
-                            <span><a href='mailto:info@tnses.org'>info@tnses.org</a></span>
-                        </li>
-                        <li>
-                            <span><i className="fa-solid fa-phone"></i> +27 12 345 67-89</span>
-                            <span>+27 12 234 56-78</span>
-                        </li>
-                    </ul>
-                </div>
-                <ul className='sci'>
-                    <li><a href=''><i className='fab fa-facebook'></i></a></li>
-                    <li><a href=''><i className='fab fa-instagram'></i></a></li>
-                    <li><a href=''><i className='fab fa-twitter'></i></a></li>
-                    <li><a href=''><i className='fab fa-youtube'></i></a></li>
-                </ul>
-            </div>
-
-            <div className='contact-form'>
-                <h2>Send a Message</h2>
-                <div className='form-box'>
-                    <div className='input-box w50'>
-                        <input type='text' name='firstName' required />
-                        <span>First Name</span>
-                    </div>
-                    <div className='input-box w50'>
-                        <input type='text' name='lastName' required />
-                        <span>Last Name</span>
-                    </div>
-                    <div className='input-box w50'>
-                        <input type='email' name='email' required />
-                        <span>Email Address</span>
-                    </div>
-                    <div className='input-box w50'>
-                        <input type='tel' name='mobile' required />
-                        <span>Mobile Number</span>
-                    </div>
-                    <div className='input-box w100'>
-                        <textarea name='message' required></textarea>
-                        <span>Write your message here</span>
-                    </div>
-                    <div className='input-box w100'>
-                        <input type='submit' value='Send' />
-                    </div>
-                </div>
-            </div>
+      <div className='contact-container'>
+        <div className='contact-info'>
+          <div className='footer-col'>
+            <h4>Contact Info</h4>
+            <ul className='contact-icons'>
+              <li>
+                <i className="fa-solid fa-location-dot"></i>
+                Belhar, Pentech, Cape Town, Western Cape 7493
+              </li>
+              <li>
+                <i className="fa-solid fa-envelope"></i>
+                <a href='mailto:info@tnses.org'>info@tnses.org</a>
+              </li>
+              <li>
+                <i className="fa-solid fa-phone"></i>
+                +27 12 345 67-89
+              </li>
+            </ul>
+          </div>
+          <ul className='sci'>
+            <li><a href=''><i className='fab fa-facebook'></i></a></li>
+            <li><a href=''><i className='fab fa-instagram'></i></a></li>
+            <li><a href=''><i className='fab fa-twitter'></i></a></li>
+            <li><a href=''><i className='fab fa-youtube'></i></a></li>
+          </ul>
         </div>
+
+        <div className='contact-form'>
+          <h2>Send a Message</h2>
+          <div className='form-box'>
+            <div className='input-box w50'>
+              <input
+                type='text'
+                name='firstName'
+                required
+                placeholder='Enter your first name'
+              />
+              <span>First Name</span>
+            </div>
+            <div className='input-box w50'>
+              <input
+                type='text'
+                name='lastName'
+                required
+                placeholder='Enter your last name'
+              />
+              <span>Last Name</span>
+            </div>
+            <div className='input-box w50'>
+              <input
+                type='email'
+                name='email'
+                required
+                placeholder='Enter your email address'
+              />
+              <span>Email Address</span>
+            </div>
+            <div className='input-box w50'>
+              <input
+                type='tel'
+                name='mobile'
+                required
+                placeholder='Enter your mobile number'
+              />
+              <span>Mobile Number</span>
+            </div>
+            <div className='input-box w100'>
+              <textarea
+                name='message'
+                required
+                placeholder='Write your message here'
+              ></textarea>
+              <span>Write your message here</span>
+            </div>
+            <div className='input-box w100'>
+              <input type='submit' value='Send' />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
 export default Contact;
--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import "./Footer.css";
-import Logo from '../../assets/logo.png';
 
 const Footer = () => {
   return (
     <footer>
       <div className='footer-container'>
         <div className='row'>
+
+          {/* 1st Column */}
           <div className='footer-col'>
-            <div className='about-us'>
-              <div className='footer-logo'>
-                <img 
-                  src={Logo}
-                  alt='Logo'
-                />
-                <h1>TNSES</h1>
-              </div>
-              <h4 className='newsletter-heading'>
+            <div className='newsletter'>
+              <h4>
                 Subscribe to our newsletter
               </h4>
-              <p className='newsletter-paragraph'>
-                Receive updates, tutorials, discounts sent straight in your inbox every month.
+              <p>
+                Receive updates, tutorials, discounts sent straight to your inbox every month.
               </p>
               <form action=''>
                 <input type='email' placeholder='Email' className='input-email'></input>
@@ -31,33 +25,27 @@ const Footer = () => {
 
           {/* 2nd Column */}
           <div className='footer-col'>
-            <h4>Quick Links</h4>
-            <ul>
-              <li><a href=''>Home</a></li>
-              <li><a href=''>About</a></li>
-              <li><a href=''>Contact</a></li>
-              <li><a href=''>Team</a></li>
-            </ul>
+            <div className='quick-links'>
+              <h4>Quick Links</h4>
+              <ul>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>About</a></li>
+                <li><a href=''>Contact</a></li>
+                <li><a href=''>Team</a></li>
+              </ul>
+            </div>
           </div>
 
           {/* 3rd Column */}
           <div className='footer-col'>
-            <h4>Services</h4>
-            <ul>
-              <li><a href=''>Google for Education</a></li>
-              <li><a href=''>Blueprint</a></li>
-              <li><a href=''>Mentorship</a></li>
-            </ul>
-          </div>
-
-          {/* 4th Column */}
-          <div className='footer-col'>
-            <h4>Contact</h4>
-            <ul className='contact-icons'>
-              <li><i className="fa-solid fa-location-dot"></i>Belhar, Pentech, Cape Town, Western Cape 7493</li>
-              <li><i className="fa-solid fa-envelope"></i><a href = 'mailto:info@tnses.org'>info@tnses.org</a></li>
-              <li><i className="fa-solid fa-phone"></i>+27 12 345 67-89</li>
-            </ul>
+            <div className='services'>
+              <h4>Services</h4>
+              <ul>
+                <li><a href=''>Google for Education</a></li>
+                <li><a href=''>Blueprint</a></li>
+                <li><a href=''>Mentorship</a></li>
+              </ul>
+            </div>
           </div>
         </div>
 
@@ -66,13 +54,6 @@ const Footer = () => {
             <p>
               &#169; 2024 TNSES. All Rights Reserved.
             </p>
-          </div>
-
-          <div className='social-icons'>
-              <a href=''><i className='fab brands fa-facebook'></i></a>
-              <a href=''><i className='fab brands fa-instagram'></i></a>
-              <a href=''><i className='fab brands fa-twitter'></i></a>
-              <a href=''><i className='fab brands fa-youtube'></i></a>
           </div>
       </div>
     </footer>
